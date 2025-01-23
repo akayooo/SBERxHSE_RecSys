e76a09c407aa4c654f423b1137d931c1dd9a0607
Никита Нестеров <145280983+akayooo@users.noreply.github.com>
2025-01-23 22:47:09 +0300

    Delete evaluation.py

--- evaluation.py
+++
@@ -1,6 +0,0 @@
-from utils import ndcg_at_k
-
-def evaluate_model(model, test_data, features, target):
-    predictions = model.predict(test_data[features])
-    ndcg_at_10 = ndcg_at_k(test_data[target].values, predictions, k=10)
-    print(f"NDCG@10: {ndcg_at_10}")
